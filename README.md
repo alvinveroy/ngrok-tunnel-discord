f817d8569d3cdc961c3a401cecac5c1d87ba08a1
Alvin Veroy <me@alvin.tech>
2021-10-11 20:24:56 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Raspberry Pi Ngrok ssh tunnel Discord Announcer.
+
+Place ngrok.service to /etc/systemd/system/
---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const ngrok = require('ngrok');
+const { Webhook, } = require('discord-webhook-node');
+const hook = new Webhook(process.env.DISCORD_WEBHOOK);
+
+const ssh = async _ => {
+    const url = await ngrok.connect({
+        proto : 'tcp',
+        addr : 22,
+        authtoken: process.env.AUTH,
+    });    
+    return url;
+};
+
+const IMAGE_URL = 'https://cdn.discordapp.com/icons/674207974016811028/2542f2fbcc434a47f2d3139ad850c211.png';
+hook.setUsername('Server Tunnel');
+hook.setAvatar(IMAGE_URL);
+
+ssh()
+.then(data => { 
+    hook.send(data);
+    console.log('Tunnel Address: ' + data);
+})
+.catch(err => console.log(err));
