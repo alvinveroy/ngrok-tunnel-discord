a282b92154790e551bcc7e50e3ec97f66211c3cc
Alvin Veroy <me@alvin.tech>
2021-10-12 22:52:07 +0800

    add additional port

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const ngrok = require('ngrok');
 const { Webhook, } = require('discord-webhook-node');
 const hook = new Webhook(process.env.DISCORD_WEBHOOK);
 
+const fbbot = async _ => {
+    const url = await ngrok.connect(5000);
+    return url;
+}
 const ssh = async _ => {
     const url = await ngrok.connect({
         proto : 'tcp',
@@ -17,6 +21,8 @@ hook.setUsername('Server Tunnel');
 hook.setAvatar(IMAGE_URL);
 
 ssh()
+fbbot()
+
 .then(data => { 
     hook.send(data);
     console.log('Tunnel Address: ' + data);
