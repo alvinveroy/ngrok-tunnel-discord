434774f815fb872dfadea58b110f7d25e671a14d
Alvin Veroy <me@alvin.tech>
2021-10-12 22:58:18 +0800

    add additional port hotfix

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ const ngrok = require('ngrok');
 const { Webhook, } = require('discord-webhook-node');
 const hook = new Webhook(process.env.DISCORD_WEBHOOK);
 
-const fbbot = async _ => {
-    const url = await ngrok.connect(5000);
-    return url;
-}
+const fbbot = ngrok.connect(5000);
 const ssh = async _ => {
     const url = await ngrok.connect({
         proto : 'tcp',
@@ -21,7 +18,6 @@ hook.setUsername('Server Tunnel');
 hook.setAvatar(IMAGE_URL);
 
 ssh()
-fbbot()
 
 .then(data => { 
     hook.send(data);
